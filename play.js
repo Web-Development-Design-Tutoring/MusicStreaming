2bb5d2b1173b3623f6181ee1e4d072f1fde47168
Camron Braden <CamronRBraden@gmail.com>
2021-09-30 19:04:39 -0500

    stuff

---
+++ play.js
@@ -0,0 +1,18 @@
+function getRandomInt(max) {
+   return Math.floor(Math.random() * max);
+ }
+
+ var numWeNeed = document.write(getRandomInt(4));
+
+if (numWeNeed == 0) {
+  
+} else if (numWeNeed == 1) {
+  
+} else if (numWeNeed == 2) {
+  
+} else if (numWeNeed == 3) {
+  
+}
+ //gets a random number from 1-10
+ 
+ 
