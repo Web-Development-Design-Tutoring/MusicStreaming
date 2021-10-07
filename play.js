065283d6035dc2fe9a3882d12c56ac4ffb4e08b3
Camron Braden <CamronRBraden@gmail.com>
2021-10-07 18:12:33 -0500

    test push

--- play.js
+++ play.js
@@ -2,7 +2,21 @@ function getRandomInt(max) {
    return Math.floor(Math.random() * max);
  }
 
- var numWeNeed = document.write(getRandomInt(4));
+ var numWeNeed = document.write(getRandomInt(10));
+ 
+ var crayons = new Audio('songsForProject/Crayons Can Melt on Us for All I Care.mp3');
+ var fake = new Audio('songsForProject/Fake Rickroll to scare your friends.mp3');
+ var ginger = new Audio('songsForProject/Ginger Root - Loretta.mp3');
+ var pirate = new Audio('songsForProject/He is a Pirate.mp3');
+ var flamingo = new Audio('songsForProject/Kero Kero Bonito - Flamingo.mp3');
+ var rick = new Audio('songsForProject/Rick Astley - Never Gonna Give You Up (Official Music Video).mp3');
+ var smash = new Audio('songsForProject/Smash Mouth _ All Star (HQ).mp3');
+ var space = new Audio('songsForProject/Space Jam.mp3');
+ var unravel = new Audio('songsForProject/Unravel (Acoustic Version).mp3');
+ var baka = new Audio('songsForProject/Yakuza OST - Baka Mitai (ばかみたい) Kiryu full version.mp3');
+
+
+
 
 if (numWeNeed == 0) {
   
@@ -11,6 +25,18 @@ if (numWeNeed == 0) {
 } else if (numWeNeed == 2) {
   
 } else if (numWeNeed == 3) {
+  
+} else if (numWeNeed == 4) {
+  
+} else if (numWeNeed == 5) {
+  
+} else if (numWeNeed == 6) {
+  
+} else if (numWeNeed == 7) {
+  
+} else if (numWeNeed == 8) {
+  
+} else if (numWeNeed == 9) {
   
 }
  //gets a random number from 1-10
